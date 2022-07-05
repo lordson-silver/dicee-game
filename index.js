0822f3164eeb45bea848db277bfdcabeffea86e0
lordson-silver <lordsonochuko5@gmail.com>
2022-07-05 22:06:02 +0100

    Used Template Literal

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function game() {
   let a = Math.floor(Math.random() * 6 + 1);
   let b = Math.floor(Math.random() * 6 + 1);
-  const randomdie1 = ("images/dice" + a + ".png");
-  const randomdie2 = ("images/dice" + b + ".png");
+  //const randomdie1 = ("images/dice" + a + ".png");
+  const randomdie1 = `images/dice${a}.png`;
+  const randomdie2 = `images/dice${b}.png`;
+  //const randomdie2 = ("images/dice" + b + ".png");
   document.querySelector(".img1").setAttribute("src", randomdie1);
   document.querySelector(".img2").setAttribute("src", randomdie2);
 
